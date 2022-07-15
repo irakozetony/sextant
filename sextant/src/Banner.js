14b80ab04004e90015adc4346b21d95a8e968659
Txny <irakozeyves9@gmail.com>
2022-07-15 05:06:27 +0200

    created my first components. yayy

---
+++ sextant/src/Banner.js
@@ -0,0 +1,9 @@
+const Banner = () => {
+    return (
+        <div className="banner">
+            <h1>Sextant Dashboard</h1>
+        </div>
+     );
+}
+
+export default Banner;
---
+++ sextant/src/CardComponent.js
@@ -0,0 +1,17 @@
+
+const CardComponent = () => {
+    return (
+        <div className="cardcomponent">
+            <div className="header">
+                <h3>Header for my component</h3>
+            </div>
+            <div className="body">
+                <p>
+                    This section will contain other components. Child components to be specific.
+                </p>
+            </div>
+        </div>
+     );
+}
+
+export default CardComponent;
